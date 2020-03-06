bc709e71bda830928172d12f87f551b1b5cade64
jamesmmatt <jamesmmatt@ksu.edu>
2020-03-05 23:52:53 -0600

    some system call stuff

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -16,5 +16,23 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+
+//  added this: and switch statement
+  int * pointer = f->esp;
+
+  int system_call = * pointer;
+  // int sys_code = *(int*)f->esp; // gets the syscall code
+
+	switch (system_call)
+	{
+		case SYS_WRITE: // write to a file
+		printf("fd : %d | Length : %d\n",*(pointer + 5),*(pointer + 7));
+		printf("buffer: %s\n",*(pointer+6));
+		break;
+
+		default:
+		printf("No match\n");
+	}
+
   thread_exit ();
 }
